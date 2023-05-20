4b34b66e5cdbc083fd1f3a0d134a7f3d70f76ed7
Rooban <126929293+coderAI01@users.noreply.github.com>
2023-05-20 11:51:02 +0530

    com.example.bean

---
+++ SimpleBean.java
@@ -0,0 +1,40 @@
+package com.example.bean;
+
+public class SimpleBean {
+	private String customer_name;
+	private String age;
+	private String principle_amount;
+	private String years;
+	private String rate;
+	public String getCustomer_name() {
+		return customer_name;
+	}
+	public void setCustomer_name(String customer_name) {
+		this.customer_name = customer_name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getPrinciple_amount() {
+		return principle_amount;
+	}
+	public void setPrinciple_amount(String principle_amount) {
+		this.principle_amount = principle_amount;
+	}
+	public String getYears() {
+		return years;
+	}
+	public void setYears(String years) {
+		this.years = years;
+	}
+	public String getRate() {
+		return rate;
+	}
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+}	
+
